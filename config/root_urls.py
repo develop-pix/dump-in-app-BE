061f2e4a83f09bf2a213c572d01fadfe7be0deae
Zamoca <suntail2002@naver.com>
2023-10-25 00:34:56 +0900

    Feat: 프로메테우스 연결을 위한 장고 설정 적용
    - 프로메테우스 모니터링 설정을 위해 장고 설정에서 미들웨어와 서드파티 설정 적용
    
    Related to #2

--- config/root_urls.py
+++ config/root_urls.py
@@ -1,10 +1,12 @@
 from django.conf import settings
 from django.contrib import admin
 from django.conf.urls.static import static
-from django.urls import path
+from django.urls import include, path
 
 
-web_urlpatterns = []
+web_urlpatterns = [
+    path('', include('django_prometheus.urls')),
+]
 
 admin_urlpatterns = [
     path("admin/", admin.site.urls),
