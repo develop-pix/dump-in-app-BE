7cef41df1eddc9649f4848c913d28da155f7e44d
SaJH <wogur981208@gmail.com>
2023-11-16 05:44:51 +0900

    CI: CI TEST

--- config/django/base.py
+++ config/django/base.py
@@ -85,7 +85,7 @@ if os.environ.get("GITHUB_WORKFLOW"):
             "NAME": "github_actions",
             "USER": "postgres",
             "PASSWORD": "password",
-            "HOST": "localhost",
+            "HOST": "postgres",
             "PORT": "5432",
         }
     }
