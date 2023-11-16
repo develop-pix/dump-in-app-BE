e9bd94c2f84062a6404f34c5e5e68c34242a7d40
SaJH <wogur981208@gmail.com>
2023-11-16 18:21:09 +0900

    CI: CI TEST

--- tests/users/test_managers.py
+++ tests/users/test_managers.py
@@ -63,4 +63,3 @@ class TestUserManager:
         assert user.username == username
         assert user.user_social_provider_id == UserProvider.EMAIL.value
         assert user.groups.first().id == AuthGroup.ADMIN.value
-
