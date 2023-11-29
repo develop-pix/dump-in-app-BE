41ad9398362f20e770e7269846eb38f7321e8109
SaJH <wogur981208@gmail.com>
2023-11-29 17:53:38 +0900

    Feat: Nofound, PermissionDenied Exception 추가 #13
    - 리뷰 예외처리에서 사용하기 위함
    Related to #13

--- dump_in/common/exception/exceptions.py
+++ dump_in/common/exception/exceptions.py
@@ -25,3 +25,15 @@ class ValidationException(BaseAPIException):
     status_code = status.HTTP_400_BAD_REQUEST
     default_detail = "Validation failed"
     code = 1003
+
+
+class NotFoundException(BaseAPIException):
+    status_code = status.HTTP_404_NOT_FOUND
+    default_detail = "Not found"
+    code = 1004
+
+
+class PermissionDeniedException(BaseAPIException):
+    status_code = status.HTTP_403_FORBIDDEN
+    default_detail = "Permission denied"
+    code = 1005
