40aaaa06f2bf384420cf4875485d1e29a05e6a70
SaJH <wogur981208@gmail.com>
2023-11-29 17:48:52 +0900

    Remove: 이미지 S3에 전송하는 API 삭제 #13
    - 서버에서 이미지를 처리하기에 부하와 시간이 오래 걸려 프론트에서 처리하기로 합의
    
    Related to #13

--- dump_in/images/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from dump_in.images.apis import ImageUploadAPI
-
-urlpatterns = [
-    path("upload", ImageUploadAPI.as_view(), name="upload"),
-]
--- dump_in/images/utils.py
+++
@@ -1,3 +0,0 @@
-def bytes_to_mib(value: int) -> float:
-    # 1 bytes = 9.5367431640625E-7 mebibytes
-    return value * 9.5367431640625e-7
