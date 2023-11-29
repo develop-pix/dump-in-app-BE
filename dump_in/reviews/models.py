8896d2a739f18988360e83a8e049d5dc1665eaf4
SaJH <wogur981208@gmail.com>
2023-11-29 17:47:57 +0900

    Feat: Review 모델 추가 #13
    - 리뷰, 리뷰 좋아요, 해시태그, 리뷰이미지 모델 생성
    Related to #13

---
+++ dump_in/reviews/models.py
@@ -0,0 +1,67 @@
+from django.db import models
+
+from dump_in.common.base.models import BaseModel, SimpleModel
+from dump_in.photo_booths.models import PhotoBooth
+from dump_in.users.models import User
+
+
+class HashTag(SimpleModel):
+    def __str__(self):
+        return f"[{self.id}]: {self.name}"
+
+    class Meta:
+        db_table = "hashtag"
+        verbose_name = "hashtag"
+        verbose_name_plural = "hashtags"
+
+
+class Review(BaseModel):
+    content = models.TextField()
+    is_deleted = models.BooleanField(default=False)
+    date = models.DateField()
+    frame_color = models.CharField(max_length=8)
+    participants = models.IntegerField()
+    camera_shot = models.CharField(max_length=8)
+    goods_amount = models.BooleanField(null=True)
+    curl_amount = models.BooleanField(null=True)
+    is_public = models.BooleanField(default=False)
+    view_count = models.PositiveIntegerField(default=0)
+    like_count = models.PositiveIntegerField(default=0)
+    hashtags = models.ManyToManyField(HashTag, related_name="reviews")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="reviews")
+    photo_booth = models.ForeignKey(PhotoBooth, on_delete=models.CASCADE, related_name="reviews")
+    user_review_like_logs = models.ManyToManyField(User, through="UserReviewLikeLog", related_name="review_like_logs")
+
+    def __str__(self):
+        return f"[{self.id}]: {self.content}"
+
+    class Meta:
+        db_table = "review"
+        verbose_name = "review"
+        verbose_name_plural = "reviews"
+
+
+class ReviewImage(BaseModel):
+    review_image_url = models.URLField()
+    review = models.ForeignKey(Review, on_delete=models.CASCADE, related_name="review_images")
+
+    def __str__(self):
+        return f"[{self.id}]: {self.review_image_url}"
+
+    class Meta:
+        db_table = "review_image"
+        verbose_name = "review image"
+        verbose_name_plural = "review images"
+
+
+class UserReviewLikeLog(BaseModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    review = models.ForeignKey(Review, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"[{self.id}]: {self.user} - {self.review}"
+
+    class Meta:
+        db_table = "user_review_like_log"
+        verbose_name = "user review like log"
+        verbose_name_plural = "user review like logs"
