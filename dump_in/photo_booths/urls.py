8b8865ad1adde0a8ed2f7d4ab6d59ec23639a22d
SaJH <wogur981208@gmail.com>
2023-12-13 15:23:31 +0900

    Feat: 포토부스 API 작성 #19
    - 포토부스 지점 위치 검색 조회 API
    - 포토부스 지점 위치 리스트 조회 API
    - 포토부스 지점 상세 조회 API
    - 포토부스 지점 좋아요 API
    - 포토부스 리뷰 리스트 조회 API
    Related to #19

--- dump_in/photo_booths/urls.py
+++ dump_in/photo_booths/urls.py
@@ -1,7 +1,19 @@
 from django.urls import path
 
-from dump_in.photo_booths.apis import PhotoBoothBrandListAPI
+from dump_in.photo_booths.apis import (
+    PhotoBoothBrandListAPI,
+    PhotoBoothDetailAPI,
+    PhotoBoothLikeAPI,
+    PhotoBoothLocationListAPI,
+    PhotoBoothLocationSearchAPI,
+    PhotoBoothReviewListAPI,
+)
 
 urlpatterns = [
     path("brands", PhotoBoothBrandListAPI.as_view(), name="photo-booth-brand-list"),
+    path("locations/search", PhotoBoothLocationSearchAPI.as_view(), name="photo-booth-location-search"),
+    path("locations", PhotoBoothLocationListAPI.as_view(), name="photo-booth-location-list"),
+    path("<str:photo_booth_id>", PhotoBoothDetailAPI.as_view(), name="photo-booth-detail"),
+    path("<str:photo_booth_id>/likes", PhotoBoothLikeAPI.as_view(), name="photo-booth-like"),
+    path("<str:photo_booth_id>/reviews", PhotoBoothReviewListAPI.as_view(), name="photo-booth-review-list"),
 ]
