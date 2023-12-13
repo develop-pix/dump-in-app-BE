f43bd1d19ee8f05a2ebf06fe2cf25e6df06164e2
SaJH <wogur981208@gmail.com>
2023-12-13 15:15:10 +0900

    Test: 포토부스 업체 리스트 조회 API 테스트 코드 작성 #19
    Related to #19

--- tests/photo_booths/apis/test_photo_booth_brand_list.py
+++ tests/photo_booths/apis/test_photo_booth_brand_list.py
@@ -1,21 +1,24 @@
+import pytest
 from django.urls import reverse
 
 from tests.utils import IsAuthenticateTestCase
+
+pytestmark = pytest.mark.django_db
 
 
 class TestPhotoBoothBrandListAPI(IsAuthenticateTestCase):
     url = reverse("api-photo-booths:photo-booth-brand-list")
 
-    def test_photo_booth_brand_list_get_success(self, photo_booth_brand, new_users):
-        access_token = self.obtain_token(new_users)
+    def test_photo_booth_brand_list_get_success(self, photo_booth_brand_list, valid_user):
+        access_token = self.obtain_token(valid_user)
         self.authenticate_with_token(access_token)
         response = self.client.get(self.url)
 
         assert response.status_code == 200
-        assert response.data["data"] is not None
+        assert len(response.data["data"]) == 3
 
     def test_photo_booth_brand_list_get_fail_not_authenticated(self):
         response = self.client.get(self.url)
 
         assert response.status_code == 401
-        assert response.data["message"] == "자격 인증데이터(authentication credentials)가 제공되지 않았습니다."
+        assert response.data["message"] == "Authentication credentials were not provided."
