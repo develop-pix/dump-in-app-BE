68feea9d58360714ecedc5ded81a234238fa6304
SaJH <wogur981208@gmail.com>
2023-12-14 10:06:01 +0900

    Feat: Photo Booth Event 관련 API 작성 #22
    Related to #22

--- dump_in/photo_booths/urls.py
+++ dump_in/photo_booths/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
 from dump_in.photo_booths.apis import (
+    PhotoBoothBrandDetailAPI,
+    PhotoBoothBrandEventListAPI,
     PhotoBoothBrandListAPI,
+    PhotoBoothBrandReviewListAPI,
     PhotoBoothDetailAPI,
     PhotoBoothLikeAPI,
     PhotoBoothLocationListAPI,
@@ -11,6 +14,9 @@ from dump_in.photo_booths.apis import (
 
 urlpatterns = [
     path("brands", PhotoBoothBrandListAPI.as_view(), name="photo-booth-brand-list"),
+    path("brands/<int:photo_booth_brand_id>", PhotoBoothBrandDetailAPI.as_view(), name="photo-booth-brand-detail"),
+    path("brands/<int:photo_booth_brand_id>/events", PhotoBoothBrandEventListAPI.as_view(), name="photo-booth-brand-event-list"),
+    path("brands/<int:photo_booth_brand_id>/reviews", PhotoBoothBrandReviewListAPI.as_view(), name="photo-booth-brand-review-list"),
     path("locations/search", PhotoBoothLocationSearchAPI.as_view(), name="photo-booth-location-search"),
     path("locations", PhotoBoothLocationListAPI.as_view(), name="photo-booth-location-list"),
     path("<str:photo_booth_id>", PhotoBoothDetailAPI.as_view(), name="photo-booth-detail"),
