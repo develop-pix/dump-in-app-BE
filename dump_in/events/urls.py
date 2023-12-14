5b4a44c20d9a84e941f0a02b2206daebb86a4b8c
SaJH <wogur981208@gmail.com>
2023-12-14 10:04:33 +0900

    Feat: Event 관련 API 작성 #22
    Related to #22

---
+++ dump_in/events/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from dump_in.events.apis import EventDetailAPI, EventLikeAPI, EventListAPI
+
+urlpatterns = [
+    path("", EventListAPI.as_view(), name="event-list"),
+    path("<int:event_id>", EventDetailAPI.as_view(), name="event-detail"),
+    path("<int:event_id>/likes", EventLikeAPI.as_view(), name="event-like"),
+]
