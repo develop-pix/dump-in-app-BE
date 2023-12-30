0b59337e41569ddc7a090e7bbf6edae66c83a4b4
SaJH <wogur981208@gmail.com>
2023-12-30 19:36:10 +0900

    Feat: 애플 로그인 API 플로우 수정 # 30
    - 기존 백엔드 서버가 처리를 했지만 클라이언트에서 보내준 토큰만 decode를 하여 DB에 저장 후 JWT반환으로 변경
    - 모바일 Oauth 로 인하여 변경
    Related to #30

--- dump_in/authentication/urls.py
+++ dump_in/authentication/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 
 from dump_in.authentication.apis import (
     AppleLoginAPI,
-    AppleLoginRedirectAPI,
     KakaoLoginAPI,
     NaverLoginAPI,
     UserJWTRefreshAPI,
@@ -14,6 +13,5 @@ urlpatterns = [
     # oauth
     path("kakao/login", KakaoLoginAPI.as_view(), name="kakao-login"),
     path("naver/login", NaverLoginAPI.as_view(), name="naver-login"),
-    path("apple/callback", AppleLoginAPI.as_view(), name="apple-login-callback"),
-    path("apple/redirect", AppleLoginRedirectAPI.as_view(), name="apple-login-redirect"),
+    path("apple/login", AppleLoginAPI.as_view(), name="apple-login"),
 ]
