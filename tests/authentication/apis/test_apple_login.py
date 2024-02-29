564398d4ad3c15777fee882f36d3598e444fa58b
SaJH <wogur981208@gmail.com>
2024-02-29 18:01:23 +0900

    Test: 소셜로그인 모바일 토큰 null 허용 관련 테스트 코드 수정 #41
    Related to #41

--- tests/authentication/apis/test_apple_login.py
+++ tests/authentication/apis/test_apple_login.py
@@ -28,6 +28,30 @@ class TestAppleLogin:
         assert response.data["data"]["access_token"] is not None
         assert response.data["data"]["refresh_token"] is not None
 
+    def test_apple_login_api_post_success_mobile_token_none(self, api_client, mocker, user_social_provider, group):
+        user_info_response = {
+            "sub": "001",
+            "email": "test_email",
+        }
+
+        mocker.patch("dump_in.authentication.services.apple_oauth.AppleLoginFlowService.get_user_info", return_value=user_info_response)
+
+        response = api_client.post(
+            path=self.url,
+            data={
+                "identify_token": "test_token",
+                "mobile_token": None,
+            },
+            format="json",
+        )
+
+        assert response.status_code == 200
+        assert response.data["code"] == "request_success"
+        assert response.data["success"] is True
+        assert response.data["message"] == "Request was successful."
+        assert response.data["data"]["access_token"] is not None
+        assert response.data["data"]["refresh_token"] is not None
+
     def test_apple_login_api_post_fail_identify_token_required(self, api_client):
         response = api_client.post(path=self.url)
 
--- tests/authentication/apis/test_kakao_login.py
+++ tests/authentication/apis/test_kakao_login.py
@@ -31,6 +31,33 @@ class TestKakaoLogin:
         assert response.data["data"]["access_token"] is not None
         assert response.data["data"]["refresh_token"] is not None
 
+    def test_kakao_login_api_post_success_mobile_token_none(self, api_client, mocker, user_social_provider, group):
+        user_info_response = {
+            "id": 123456789,
+            "kakao_account": {
+                "profile": {"nickname": "test"},
+                "email": "test@test.com",
+            },
+        }
+
+        mocker.patch("dump_in.authentication.services.kakao_oauth.KakaoLoginFlowService.get_user_info", return_value=user_info_response)
+
+        response = api_client.post(
+            path=self.url,
+            data={
+                "access_token": "access_token",
+                "mobile_token": None,
+            },
+            format="json",
+        )
+
+        assert response.status_code == 200
+        assert response.data["code"] == "request_success"
+        assert response.data["success"] is True
+        assert response.data["message"] == "Request was successful."
+        assert response.data["data"]["access_token"] is not None
+        assert response.data["data"]["refresh_token"] is not None
+
     def test_kakao_login_api_post_fail_access_token_required(self, api_client):
         response = api_client.post(path=self.url)
 
