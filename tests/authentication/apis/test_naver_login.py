f4217c0c46be787395b5c972630470aa7d53ad22
Sa Potato <wogur981208@gmail.com>
2024-02-29 18:12:15 +0900

    gh-41: Fix: 소셜로그인 시 모바일 토큰 null 허용 및 관련 테스트 코드 수정 (#42)
    - 소셜로그인 시 모바일 토큰 null 허용
    - 관련 테스트 코드 수정
    - API 문서 수정

--- tests/authentication/apis/test_naver_login.py
+++ tests/authentication/apis/test_naver_login.py
@@ -32,6 +32,34 @@ class TestNaverLogin:
         assert response.data["data"]["access_token"] is not None
         assert response.data["data"]["refresh_token"] is not None
 
+    def test_naver_login_api_success_mobile_token_none(self, api_client, mocker, user_social_provider, group):
+        user_info_response = {
+            "id": 123456789,
+            "nickname": "test_nickname",
+            "email": "test@test.com",
+            "gender": "F",
+            "birthyear": "2023",
+            "birthday": "01-01",
+        }
+
+        mocker.patch("dump_in.authentication.services.naver_oauth.NaverLoginFlowService.get_user_info", return_value=user_info_response)
+
+        response = api_client.post(
+            path=self.url,
+            data={
+                "access_token": "access_token",
+                "mobile_token": None,
+            },
+            format="json",
+        )
+
+        assert response.status_code == 200
+        assert response.data["code"] == "request_success"
+        assert response.data["success"] is True
+        assert response.data["message"] == "Request was successful."
+        assert response.data["data"]["access_token"] is not None
+        assert response.data["data"]["refresh_token"] is not None
+
     def test_naver_login_api_fail_access_token_required(self, api_client):
         response = api_client.post(path=self.url)
 
