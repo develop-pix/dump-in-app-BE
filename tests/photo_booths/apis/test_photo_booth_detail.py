aa8550d507d972a29351a239af42cc2cd37317ce
SaJH <wogur981208@gmail.com>
2024-02-29 17:34:24 +0900

    Test: 포토부스 지점 상세 조회 API 테스트 코드 수정 #39
    - 위치 정보 필수값 해제에 따른 테스트 코드 수정
    Related to #39

--- tests/photo_booths/apis/test_photo_booth_detail.py
+++ tests/photo_booths/apis/test_photo_booth_detail.py
@@ -78,7 +78,7 @@ class TestPhotoBoothDetail(IsAuthenticateTestCase):
         assert response.data["data"]["photo_booth_brand"]["hashtag"][0]["id"] == photo_booth.photo_booth_brand.hashtag.all()[0].id
         assert response.data["data"]["photo_booth_brand"]["hashtag"][0]["name"] == photo_booth.photo_booth_brand.hashtag.all()[0].name
 
-    def test_photo_booth_detail_get_fail_latitude_required(self, photo_booth, valid_user):
+    def test_photo_Booth_detail_get_success_location_is_none(self, photo_booth, valid_user):
         access_token = self.obtain_token(valid_user)
         self.authenticate_with_token(access_token)
         response = self.client.get(
@@ -88,33 +88,29 @@ class TestPhotoBoothDetail(IsAuthenticateTestCase):
                     "photo_booth_id": photo_booth.id,
                 },
             ),
-            data={
-                "longitude": 126.97151987127677,
-            },
         )
 
-        assert response.status_code == 400
-        assert response.data["code"] == "invalid_parameter_format"
-        assert response.data["message"] == {"latitude": ["This field is required."]}
-
-    def test_photo_booth_detail_get_fail_longitude_required(self, photo_booth, valid_user):
-        access_token = self.obtain_token(valid_user)
-        self.authenticate_with_token(access_token)
-        response = self.client.get(
-            path=reverse(
-                "api-photo-booths:photo-booth-detail",
-                kwargs={
-                    "photo_booth_id": photo_booth.id,
-                },
-            ),
-            data={
-                "latitude": 37.55558726825372,
-            },
+        assert response.status_code == 200
+        assert response.data["data"]["id"] == photo_booth.id
+        assert response.data["data"]["name"] == photo_booth.name
+        assert response.data["data"]["latitude"] == float(photo_booth.latitude)
+        assert response.data["data"]["longitude"] == float(photo_booth.longitude)
+        assert response.data["data"]["street_address"] == photo_booth.street_address
+        assert response.data["data"]["road_address"] == photo_booth.road_address
+        assert response.data["data"]["operation_time"] == photo_booth.operation_time
+        assert not response.data["data"]["is_liked"]
+        assert response.data["data"]["photo_booth_brand"]["name"] == photo_booth.photo_booth_brand.name
+        assert (
+            response.data["data"]["photo_booth_brand"]["image"][0]["id"]
+            == photo_booth.photo_booth_brand.photo_booth_brand_image.all()[0].id
         )
-
-        assert response.status_code == 400
-        assert response.data["code"] == "invalid_parameter_format"
-        assert response.data["message"] == {"longitude": ["This field is required."]}
+        assert (
+            response.data["data"]["photo_booth_brand"]["image"][0]["image_url"]
+            == photo_booth.photo_booth_brand.photo_booth_brand_image.all()[0].photo_booth_brand_image_url
+        )
+        assert response.data["data"]["photo_booth_brand"]["hashtag"][0]["id"] == photo_booth.photo_booth_brand.hashtag.all()[0].id
+        assert response.data["data"]["photo_booth_brand"]["hashtag"][0]["name"] == photo_booth.photo_booth_brand.hashtag.all()[0].name
+        assert response.data["data"]["distance"] is None
 
     def test_photo_booth_detail_get_fail_does_not_exist(self, valid_user):
         access_token = self.obtain_token(valid_user)
